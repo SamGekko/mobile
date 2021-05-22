4662ea8eb5e3a1dfca9c90f2b3ba6fd25745864e
SamGekko <alex2002kuza@gmail.com>
2021-05-22 20:49:14 +0300

    Initial commit

---
+++ Lesson1/myapplication/src/main/java/ru/mirea/kuznetsov/myapplication/ClickButtons.java
@@ -0,0 +1,37 @@
+package ru.mirea.kuznetsov.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class ClickButtons extends AppCompatActivity {
+    private TextView textView;
+    private Button buttonOk;
+    private Button buttonCancel;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        //нахождение элементов
+        textView = (TextView) findViewById(R.id.tvOut);
+        buttonOk = (Button) findViewById(R.id.btnOk);
+        buttonCancel = (Button) findViewById(R.id.btnCancel);
+
+        View.OnClickListener oclBtnOk = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                textView.setText("Нажата кнопка ОК");
+            }
+        };
+        // присвоим обработчик кнопке OK (btnOk)
+        buttonOk.setOnClickListener(oclBtnOk);
+    }
+
+    public void onMyButtonClick(View view) {
+        Toast.makeText(this, "Ещё один способ!", Toast.LENGTH_SHORT).show();
+    }
+}
