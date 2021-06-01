bd1082bccedcd4a8e7bbf182a55c4e0de34f7935
SamGekko <alex2002kuza@gmail.com>
2021-06-01 20:34:00 +0300

    practice2

---
+++ practice2/multiactivity/src/main/java/ru/mirea/kuznetsov/multiactivity/SecondActivity.java
@@ -0,0 +1,22 @@
+package ru.mirea.kuznetsov.multiactivity;
+
+import android.os.Bundle;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import android.view.View;
+import android.widget.TextView;
+
+public class SecondActivity extends AppCompatActivity {
+
+    TextView txt;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+        txt = (TextView) findViewById(R.id.textView2);
+        String text = (String) getIntent().getSerializableExtra("key");
+        txt.setText(text);
+    }
+}
